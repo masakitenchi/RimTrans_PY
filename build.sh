3143ed02fbbd2b94db13fed4a1106dbfa47b3a4f
Manifold Paradox <masakitenchi@users.noreply.github.com>
2024-03-18 22:30:04 +0900

    Add submodule & workflow file

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 pip install pyinstaller
 pyinstaller __main__.py --windowed -n Pyscripts -y
-
-echo "zip into a zip file(y/N)?"
-read ans
-if [ "$ans" != "y" ]; then
-	echo "Aborted"
-	exit 1
-fi
-cd dist
-7z u Test.zip Pyscripts
-7z a -thash Test.zip.sha256 Test.zip
-7z u dist.zip Test.zip.sha256 Test.zip

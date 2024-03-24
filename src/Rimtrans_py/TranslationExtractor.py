a0269faaf5a2b828be0d693f202f0d17b8d96947
Manifold Paradox <masakitenchi@users.noreply.github.com>
2024-03-24 17:57:17 +0900

    Rewrite extract code
    new extract_tree method for extracting a xml tree

--- src/Rimtrans_py/TranslationExtractor.py
+++ src/Rimtrans_py/TranslationExtractor.py
@@ -1,3 +1,4 @@
+from operator import concat
 import os
 import os.path
 import lxml.etree as ET
@@ -5,6 +6,7 @@ import regex as re
 import argparse
 from typing import *
 from FileParser import BFS
+from utilities import try_add
 
 """
 Consider using ElephantTusk.tools.point.label instead of ElephantTusk.tools.1.label for translation '尖' (Items_Exotic.xml)
@@ -12,42 +14,51 @@ Consider using ElephantTusk.tools.point.label instead of ElephantTusk.tools.1.la
 where 'point' is the text of label, '1' is the index of the item in the list
 """
 
-# Find all patches that target defs with defName
+
 xpath_regex = re.compile(
     r"Defs\/(?<defType>.*?Def)\[(?<defNames>.*)\]\/(?<field>label|description)"
 )
+"""
+Find all patches that target certain def\n
+Example: Defs/ThingDef[defName="XXX"]/label \n 
+-> re.match()['defType'] = 'ThingDef' \n 
+-> re.match()['defNames'] = 'defName="XXX"' \n 
+-> re.match()['field'] = 'label'
+"""
 
 defNames_regex = re.compile(r'defName\W*?=\W*?"(?<defName>.*?)"')
+"""
+We only consider a patch valid if it targets a non-abstract def for now
+"""
 
 
 Defs_xpath = ET.XPath(
     "//*[preceding-sibling::defName or following-sibling::defName][self::label or self::description]/.."
 )
 
-Patch_xpath = '//xpath[contains(text(),"label") or contains(text(),"description") and not(contains(text(), "labelShort"))]/..'
+patch_xpath = '//xpath[contains(text(),"label") or contains(text(),"description") and not(contains(text(), "labelShort"))]/..'
 
 # lxml seems doesn't support local-name()
-Anomaly_xpath = '//*[@Class="PatchOperationAdd"]/xpath[text()="Defs"]/../value/*[not(@Abstract) or (@Abstract!="True" and @Abstract!="true")]'
+anomaly_xpath = '//*[@Class="PatchOperationAdd"]/xpath[text()="Defs"]/../value/*[not(@Abstract) or (@Abstract!="True" and @Abstract!="true")]'
+anomaly_xpath_2 = '//*[@Class="PatchOperationAdd"]/xpath[text()="Defs" or text()="/Defs" or text()="Defs/" or text()="/Defs/"]/../value/*[not(@Abstract) or (@Abstract!="True" and @Abstract!="true")]'
 
 list_translatable = ["stages", "lifeStages", "tools", "degreeDatas"]
 
 class_name_as_key = ["comps"]
 
-tag_translatable = ["label", "labelNoun", "description", "jobString"]
+tag_translatable = ["label", "labelNoun", "description", "jobString", "labelShort"]
 
 parameter_regex = re.compile(r"\s?{\d}\s?")
 thought_degree_regex = re.compile(r"\s?\(\+(?<degree>\d)\)\s?")
 
 
-def extract_list(tree: ET._Element) -> list[str]:
+def extract_list(tree: ET._Element) -> list[tuple[str, str]]:
     """
     Given an element contains translatable list, return the list of all extracted keys
     :param tree: the element containing the list
     :return: list of keys
     """
-    if tree.tag not in list_translatable:
-        return
-    keys = []
+    keys: list[tuple] = []
     index = 0
     for node in tree.iterchildren("li"):
         if node.find("./label") is not None:
@@ -59,12 +70,21 @@ def extract_list(tree: ET._Element) -> list[str]:
             if thought_degree_regex.search(label) is not None:
                 degree = thought_degree_regex.search(label)["degree"]
                 label = thought_degree_regex.sub("", label) + f"_{degree}"
-            keys.append(build_key(node, "label", label=label, index=index))
+            keys.append(
+                (
+                    build_key(node, "label", label=label, index=index),
+                    node.find("./label").text,
+                )
+            )
             if node.find("./description") is not None:
-                keys.append(build_key(node, "description", label=label, index=index))
+                keys.append(
+                    (
+                        build_key(node, "description", label=label, index=index),
+                        node.find("./description").text,
+                    )
+                )
         index += 1
     return keys
-
 
 def build_key(
     node: ET._Element, finalTag: str, *, label: Optional[str] = None, index: int
@@ -83,31 +103,85 @@ def build_key(
     return key_str
 
 
-def extract_single(element: ET._Element) -> list:
+def extract_single_def(element: ET._Element) -> dict[str, dict[str, str]]:
     """
     :param element: element
-    :returns: a list of keys
     """
-    keys = []
     if (
         element.get("Abstract")
         and element.get("Abstract").lower() == "true"
         or element.find("./defName") is None
     ):
-        return []
+        return {}
+    result = {}
     for node in element.iterchildren():
         if node.tag in list_translatable:
-            keys.extend(extract_list(node))
+            result.update(extract_list(node))
         elif node.tag in tag_translatable:
-            keys.append(f"{element.find('./defName').text}.{node.tag}")
-    return keys
+            result.update(
+                    {f"{element.find('./defName').text}.{node.tag}":
+                    node.text}
+            )
+    return {element.tag: result}
 
+def extract_single_patch(node: ET._Element) -> dict[str, dict[str, str]]:
+    """
+    :param node: patch xml element
+    """
+    if node is None: return {}
+    patches = node.xpath(patch_xpath)
+    anomaly_patches = node.xpath(anomaly_xpath)
+    result = {}
+    for patch in patches:
+        patch: ET._Element
+        xpath = patch.find("./xpath")
+        match = re.match(xpath_regex, xpath.text)
+        if match:
+            defType, defName, field = match.groups()
+        else:
+            continue
+        value = patch.find(f"./value/{field}").text
+        if defType not in result:
+            result[defType] = dict()
+        defNames = re.findall(defNames_regex, defName)
+        for defName in defNames:
+            key = f"{defName}.{field}"
+            result[defType][key] = value
+    for anomaly in anomaly_patches:
+        defName = anomaly.find("./defName").text
+        label = (
+            anomaly.find("./label").text
+            if anomaly.find("./label") is not None
+            else ""
+        )
+        description = (
+            anomaly.find("./description").text
+            if anomaly.find("./description") is not None
+            else ""
+        )
+        defType = anomaly.tag
+        if defType not in result:
+            result[defType] = dict()
+        result[defType][f"{defName}.label"] = label
+        result[defType][f"{defName}.description"] = description
+    return result
 
-def extract_tree(tree: ET._ElementTree) -> list:
+def extract_tree(tree: ET._ElementTree) -> dict[str, dict[str, str]]:
+    """
+    Given a xml tree, returns a dict of {defType : {key: value}}
+    """
     root = tree.getroot()
-    keys = []
-    for node in root.iterchildren():
-        keys.extend(extract_single(node))
+    keys = {}
+    if root.tag == 'Defs':
+        func = extract_single_def
+        nodeiter = root.iterchildren()
+    elif root.tag == 'Patch':
+        func = extract_single_patch
+        nodeiter = root.xpath(patch_xpath) + root.xpath(anomaly_xpath)
+    else:
+        return {}
+    for node in nodeiter:
+        keys.update(func(node))
     return keys
 
 
@@ -118,7 +192,7 @@ def extract(
     总提取函数
     :param list_paths: 所有文件的绝对路径列表
     :param target: 提取目标，Def或Patch
-    :return: dict[defType: str, dict[defName: str, field: str]] Similar to DefDataBase<T>, where T is the outer key in this dict
+    :return: dict[defType: str, dict[key: str, value: str]] Similar to DefDataBase<T>, where T is the outer key in this dict
     """
     pairs: dict[str, dict[str, str]] = dict()
     for file in list_paths:
@@ -128,67 +202,19 @@ def extract(
                     f"path {file} does not target a file or is not absolute"
                 )
             tree = ET.parse(file)
-            root: ET._Element = tree.getroot()
-            if root.tag == "Defs" and "Def" in target:
+            #root: ET._Element = tree.getroot()
+            if tree.getroot().tag == "Defs" and "Def" in target:
                 # only looks for non-virtual defs
-                nodes = Defs_xpath(root)
-                for node in nodes:
-                    defType = node.tag
-                    if defType not in pairs:
-                        pairs[defType] = dict()
-                    key = node.find("./defName").text
-                    pairs[defType][f"{key}.label"] = (
-                        node.find("./label").text
-                        if node.find("./label") is not None
-                        else None
-                    )
-                    pairs[defType][f"{key}.description"] = (
-                        node.find("./description").text
-                        if node.find("./description") is not None
-                        else None
-                    )
-            elif root.tag == "Patch" and "Patch" in target:
+                pairs = extract_tree(tree)
+            elif tree.getroot().tag == "Patch" and "Patch" in target:
                 # looks for all patches that targeting label or description
-                nodes = root.xpath(Patch_xpath)
-                for node in nodes:
-                    xpath = node.find("./xpath")
-                    match = re.match(xpath_regex, xpath.text)
-                    if match:
-                        defType, defName, field = match.groups()
-                    else:
-                        continue
-                    value = node.find(f"./value/{field}").text
-                    if defType not in pairs:
-                        pairs[defType] = dict()
-                    defNames = re.findall(defNames_regex, defName)
-                    # print(defNames)
-                    for defName in defNames:
-                        key = f"{defName}.{field}"
-                        pairs[defType][key] = value
-                anomaly_nodes = root.xpath(Anomaly_xpath)
-                for node in anomaly_nodes:
-                    defName = node.find("./defName").text
-                    label = (
-                        node.find("./label").text
-                        if node.find("./label") is not None
-                        else ""
-                    )
-                    description = (
-                        node.find("./description").text
-                        if node.find("./description") is not None
-                        else ""
-                    )
-                    defType = node.tag
-                    if defType not in pairs:
-                        pairs[defType] = dict()
-                    pairs[defType][f"{defName}.label"] = label
-                    pairs[defType][f"{defName}.description"] = description
-
+                pairs = extract_tree(tree)
         except Exception as e:
+            """ 
             if node is not None:
                 print(f"Error when parsing {file}, {node.tag} message: {e}")
-            else:
-                print(f"Error when parsing {file}, message: {e}")
+            else: """
+            print(f"Error when parsing {file}, message: {e}")
             continue
     return pairs
 
